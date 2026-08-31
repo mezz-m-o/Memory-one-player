4bdc2c54d74463391f6fda004a0ef6785fd4f948
mezz-m-o <mezzburr@gmail.com>
2026-08-30 21:31:55 -0700

    Add files via upload

--- Set_score.js
+++ Set_score.js
@@ -5,17 +5,21 @@ const pass=document.getElementById("pass");
 const SetScore=document.getElementById("setScore");
 const score=document.getElementById("score");
 const saveButton=document.getElementById("saveButton");
+const resetButton=document.getElementById("resetButton");
 submitButton.addEventListener("click", function(){
     if(password.value=="voli Boll"){
         pass.style.display = "none";
         SetScore.style.display = "Block";
-        saveButton.addEventListener("click", setScore);
+        saveButton.addEventListener("click", function(){
+            localStorage.setItem("low_score", score.value);
+            window.location.href = "index.html";
+        });
+        resetButton.addEventListener("click", function(){
+            localStorage.removeItem("low_score");
+            window.location.href = "index.html";
+        });
     }else{
         label.style.color = "#ff0000";
         label.textContent = "incorrect";
     };
 });
-function setScore(){
-    localStorage.setItem("low_score", score.value);
-    window.location.href = "index.html";
-}
